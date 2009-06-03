a9d260a610abb640df8c5efeda990c3d563a0b8d
Dave Merwin <dmerwin@orcasinc.com>
2009-06-03 16:01:20 -0700

    adding in final files etc

--- js/site.js
+++ js/site.js
@@ -5,24 +5,30 @@ $(document).ready(function(){
 		return false;
 	});
 	
-	$('li.sample').append('<h1>Lorem Ipsum Dolor Sit</h1><h2>Lorem Ipsum Dolor Sit</h2><h3>Lorem Ipsum Dolor Sit</h3><h4>Lorem Ipsum Dolor Sit</h4><p>Lorem ipsum dolor sit amet, <a href="http://www.twitter.com/davemerwin">consectetur adipisicing</a> elit, sed do eiusmod tempor incididunt <strong>ut labore</strong> et dolore magna aliqua. Ut enim ad <em>minim veniam</em>, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>');
+	$('li.sample').append('<div class="grid_5 omega"><h1>Lorem Ipsum Dolor Sit</h1><h2>Lorem Ipsum Dolor Sit</h2><h4>Lorem Ipsum Dolor Sit</h4><h4>Lorem Ipsum Dolor Sit</h4><p>Lorem ipsum dolor sit amet, <a href="http://www.twitter.com/davemerwin">consectetur adipisicing</a> elit, sed do eiusmod tempor incididunt <strong>ut labore</strong> et dolore magna aliqua. Ut enim ad <em>minim veniam</em>, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p><p class="top"><a href="#wrap">top</a></p></div>');
 	
-	$('.helveticalArial').prepend('<h3 class="sampleType">Helvetica &amp; Arial</h3>');
-	$('.arialblackArialblack').prepend('<h3 class="sampleType">Arial Black &amp Arial Black</h3>');
-	$('.impactImpact').prepend('<h3 class="sampleType">Impact &amp; Impact</h3>');
-	$('.lucidagrandeLucidaSansUnicode').prepend('<h3 class="sampleType">Lucida Grande &amp; Lucida Sans Unicode</h3>');
-	$('.genevaTahoma').prepend('<h3 class="sampleType">Geneva &amp; Tahoma</h3>');
-	$('.helveticaTrebuchetMS').prepend('<h3 class="sampleType">Helvetica &amp; Trebuchet MS</h3>');
-	$('.verdenaVerdena').prepend('<h3 class="sampleType">Verdena &amp; Verdena</h3>');
-	$('.genevaMSSansSerif').prepend('<h3 class="sampleType">Geneva &amp; MS Sans Serif</h3>');
-	$('.georgiaGeorgia').prepend('<h3 class="sampleType">Georgia &amp; Georgia</h3>');
-	$('.palatinoPalatino').prepend('<h3 class="sampleType">Palatino &amp; Palatino</h3>');
-	$('.timesTimesNewRoman').prepend('<h3 class="sampleType">Times &amp; Times New Roman</h3>');
-	$('.newyorkMSSerif').prepend('<h3 class="sampleType">New York &amp; MS Serif</h3>');
-	$('.couriernewCourierNew').prepend('<h3 class="sampleType">Courier New &amp; Courier New</h3>');
-	$('.monacoLucidaConsole').prepend('<h3 class="sampleType">Monaco &amp; Lucida Console</h3>');
-	$('.comicsansComicSans').prepend('<h3 class="sampleType">Comic Sans &amp; Comic Sans</h3>');
-	$('.symbolSymbol').prepend('<h3 class="sampleType">Symbol &amp; Symbol</h3>');
-	$('.webdingsWebdings').prepend('<h3 class="sampleType">Webdings &amp; Webdings</h3>');
-	$('.dingbatsDingbats').prepend('<h3 class="sampleType">Dingbats &amp; Dingbats</h3>');
+	$('.sampleList li:odd').addClass('alt');
+	
+	$('#helveticalArial').prepend('<h4 class="sampleType grid_3 alpha">Helvetica &amp; Arial</h4>');
+	$('#arialblackArialblack').prepend('<h4 class="sampleType grid_3 alpha">Arial Black &amp Arial Black</h4>');
+	$('#impactImpact').prepend('<h4 class="sampleType grid_3 alpha">Impact &amp; Impact</h4>');
+	$('#lucidagrandeLucidaSansUnicode').prepend('<h4 class="sampleType grid_3 alpha">Lucida Grande &amp; Lucida Sans Unicode</h4>');
+	$('#genevaTahoma').prepend('<h4 class="sampleType grid_3 alpha">Geneva &amp; Tahoma</h4>');
+	$('#trebuchetMSArial').prepend('<h4 class="sampleType grid_3 alpha">Trebuchet MS &amp; Arial</h4>');
+	$('#verdenaVerdena').prepend('<h4 class="sampleType grid_3 alpha">Verdena &amp; Verdena</h4>');
+	$('#genevaMSSansSerif').prepend('<h4 class="sampleType grid_3 alpha">Geneva &amp; MS Sans Serif</h4>');
+	$('#georgiaGeorgia').prepend('<h4 class="sampleType grid_3 alpha">Georgia &amp; Georgia</h4>');
+	$('#palatinoPalatino').prepend('<h4 class="sampleType grid_3 alpha">Palatino &amp; Palatino</h4>');
+	$('#timesTimesNewRoman').prepend('<h4 class="sampleType grid_3 alpha">Times &amp; Times New Roman</h4>');
+	$('#newyorkMSSerif').prepend('<h4 class="sampleType grid_3 alpha">New York &amp; MS Serif</h4>');
+	$('#couriernewCourierNew').prepend('<h4 class="sampleType grid_3 alpha">Courier New &amp; Courier New</h4>');
+	$('#monacoLucidaConsole').prepend('<h4 class="sampleType grid_3 alpha">Monaco &amp; Lucida Console</h4>');
+	$('#comicsansComicSans').prepend('<h4 class="sampleType grid_3 alpha">Comic Sans &amp; Comic Sans</h4>');
+	$('#symbolSymbol').prepend('<h4 class="sampleType grid_3 alpha">Symbol &amp; Symbol</h4>');
+	$('#webdingsWebdings').prepend('<h4 class="sampleType grid_3 alpha">Webdings &amp; Webdings</h4>');
+	$('#dingbatsDingbats').prepend('<h4 class="sampleType grid_3 alpha">Dingbats &amp; Dingbats</h4>');
+	
+	$('#default-submit').css('display', 'block');
+	
+	$('body').append('<script src="googleanlytics.js" type="text/javascript" charset="utf-8"></script>');
 });
